1f57f93da15f708b237855777b54b7c40899cec9
ImSovietBear <elijah.r223@gmail.com>
2016-02-01 16:06:19 -0500

    New sketch for testing coms with RoboRio
    A sketch that runs on a second arduino that outputs at the for pins
    based on input from serial console.

---
+++ Robo Arduino Coms Tester/ComsTesterOutput/ComsTesterOutput.ino
@@ -0,0 +1,16 @@
+#define OUT_PIN0 = 3;
+#define OUT_PIN1 = 4;
+#define OUT_PIN2 = 5;
+#define OUT_PIN3 = 6;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
