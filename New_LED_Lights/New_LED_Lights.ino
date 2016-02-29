6e20295a7e5adc34018ea1c0855ff733827aa423
Bennett Clark <736f6c@gmail.com>
2016-02-28 21:09:50 -0500

    several improvements
    -organized and modified the code for setting patterns, and added all
    available patterns
    -modified Blink() code to blink at variable frequencies
    -made receiveEvent() better; hopefully it won't break now

--- New_LED_Lights/New_LED_Lights.ino
+++ New_LED_Lights/New_LED_Lights.ino
@@ -5,11 +5,22 @@
 #define DATA_PIN 2
 #define I2C_ID 10
 
+#define OFF_PATTERN_ID 0
+#define SOLID_PATTERN_ID 1
+#define FAST_BLINK_PATTERN_ID 2
+#define SLOW_BLINK_PATTERN_ID 3
+#define FADE_PATTERN_ID 4
+#define CHASE_PATTERN_ID 5
+#define RAINBOW_PATTERN_ID 6
+
 int counter = 0;
 boolean fadeIn = true;
+boolean blinkOn = true;
 int gHue = 0;
+
 int intakeRange[2] = {0,21};
-int shooter[2] = {21, 41};
+int shooterRange[2] = {21, 41};
+
 byte lightStates[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //range1(RGB pattern) range2(RGB pattern)
 
 CRGB leds[NUM_LEDS];
@@ -17,6 +28,12 @@ CRGB leds[NUM_LEDS];
 void setup() {
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
   Serial.begin(9600);
+
+  for (int i = 0; i < NUM_LEDS; i++){
+      leds[i] = CRGB::White;
+    }
+  FastLED.show();
+  delay(1000);
   
   //begins the i2c connection.
   Wire.begin(I2C_ID);
@@ -25,23 +42,8 @@ void setup() {
 }
 
 void loop() {
-  if(lightStates[3] == 0){
-    ColorFadeInOut(CRGB(lightStates[0], lightStates[1], lightStates[2]), intakeRange[0], intakeRange[1]);
-  }
-  else if(lightStates[3] == 1){
-    StaticColor(CRGB(lightStates[0], lightStates[1], lightStates[2]), intakeRange[0], intakeRange[1]);
-  }else if(lightStates[3] == 2){
-    ChaseIn(CRGB(lightStates[0], lightStates[1], lightStates[2]), intakeRange[0], intakeRange[1]);
-  }
-  
-  if(lightStates[7] == 0){
-    ColorFadeInOut(CRGB(lightStates[4], lightStates[5], lightStates[6]), shooter[0], shooter[1]);
-  }
-  else if(lightStates[7] == 1){
-    StaticColor(CRGB(lightStates[4], lightStates[5], lightStates[6]), shooter[0], shooter[1]);
-  }else if(lightStates[7] == 2){
-    ChaseIn(CRGB(lightStates[4], lightStates[5], lightStates[6]), shooter[0], shooter[1]);
-  }
+  makePatterns(lightStates[0], lightStates[1], lightStates[2], lightStates[3], \
+               lightStates[4], lightStates[5], lightStates[6], lightStates[7])
   
   for(int i=0;i<8;i++) {
     Serial.print((String)lightStates[i]);
@@ -54,12 +56,53 @@ void loop() {
   EVERY_N_MILLISECONDS( 10 ) { IncrementCounter(); }
 }
 
+void makePatterns(int r1, int g1, int b1, int pattern1, int r2, int g2, int b2, int pattern2) {
+  switch(pattern1) {
+    case OFF_PATTERN_ID:
+      Off();
+      break;
+    case SOLID_PATTERN_ID:
+      StaticColor(CRGB(r1, g1, b1), intakeRange[0], intakeRange[1]);
+      break;
+    case BLINK_PATTERN_ID:
+      Blink(CRGB(r1, g1, b1), intakeRange[0], intakeRange[1]);
+      break;
+    case FADE_PATTERN_ID:
+      ColorFadeInOut(CRGB(r1, g1, b1), intakeRange[0], intakeRange[1]);
+      break;
+    case CHASE_PATTERN_ID:
+      ChaseIn(CRGB(r1, g1, b1), intakeRange[0], intakeRange[1]);
+      break;
+    case RAINBOW_PATTERN_ID:
+      Rainbow(gHue);
+      break;
+  }
+  switch(pattern2) {
+    case OFF_PATTERN_ID:
+      Off();
+      break;
+    case SOLID_PATTERN_ID:
+      StaticColor(CRGB(r2, g2, b2), shooterRange[0], shooterRange[1]);
+      break;
+    case BLINK_PATTERN_ID:
+      Blink(CRGB(r2, g2, b2), shooterRange[0], shooterRange[1]);
+      break;
+    case FADE_PATTERN_ID:
+      ColorFadeInOut(CRGB(r2, g2, b2), shooterRange[0], shooterRange[1]);
+      break;
+    case CHASE_PATTERN_ID:
+      ChaseIn(CRGB(r1, g1, b1), intakeRange[0], intakeRange[1]);
+      break;
+    case RAINBOW_PATTERN_ID:
+      Rainbow(gHue);
+      break;
+}
+
 void IncrementCounter() {
   counter++;
   if (counter == 100) {
     counter = 0;
     fadeIn = !fadeIn;
-    Off();
   }
 }
 
@@ -89,7 +132,6 @@ void StaticColor(CRGB color, int startLED, int endLED){
 
 void ChaseIn(CRGB color, int startLED, int endLED) {
   int i = (int) ( ( (double)counter / (double)100) * int(((endLED - startLED + 1) / 2) + 0.5));
-  Serial.print(String(i) + "\n");
   leds[startLED + i] = color;
   leds[endLED - i] = color;
 }
@@ -99,14 +141,18 @@ void Rainbow(int gHue)
   fill_rainbow( leds, NUM_LEDS, gHue, 7);
 }
 
-void Blink(CRGB color, int startLED, int endLED) {
-  if(counter <= 50){
+void Blink(CRGB color, int startLED, int endLED, int freq) {
+  if ((counter % freq) == 0) {
+    blinkOn = !blinkOn;
+  }
+
+  if (blinkOn) {
     for (int i=startLED; i<endLED+1; i++){
       leds[i] = color;
-    }  
+    }
   }
-  if(counter > 50){
-    for (int i=startLED; i<endLED+1; i++){
+  else {
+    for (int i = 0; i < NUM_LEDS; i++){
       leds[i] = 0;
     }
   }
@@ -119,8 +165,10 @@ void Off(){
 }
 
 void receiveEvent(int numBytes){
-  while(Wire.available() != 0){
-    lightStates[8 - Wire.available()] = Wire.read();
+  while(Wire.available() >= 8) { 
+    for(int i=0; i < 8; i++) { 
+      lightStates[i] = Wire.read();
+    } 
   }
 }
 
